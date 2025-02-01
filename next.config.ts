00aaf62ba6298f8584503f3ba4e5bdcd89a00f51
keane <keane7a@gmail.com>
2025-02-01 15:12:15 +0000

    build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/gh-pages-test",
 };
 
 export default nextConfig;
